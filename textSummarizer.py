67cebe66f8ed9acbbfcf6a3dc4bc58967fa8e6bd
Pankhuri <36470694+Pankhuri1999@users.noreply.github.com>
2020-12-04 23:29:30 +0530

    Update textSummarizer.py

--- textSummarizer.py
+++ textSummarizer.py
@@ -6,15 +6,6 @@ import nltk
 
 #from nltk.stem import PorterStemmer
 
-"""
-For example, the Center for a New American Dream envisions "... a focus on more of what really matters, such as creating a meaningful life, contributing to community and society, valuing nature, and spending time with family and friends."
-"""
-
-"""
-At Text we want to publish books that make a difference to people’s lives. We believe that reading should be a marvellous experience, that every book you read should somehow change your life if only by a fraction. We love the phrase ‘lost in a book’—that’s where we want our readers to be. You can’t get lost in a newspaper or a magazine or even a movie. But people get lost in books every day—on the tram, on the beach, in bed. Reading is what keeps the imagination supple and challenges preconceptions and prejudices. You read at your own speed, and the world you enter courtesy of the writer is yours and yours only, even if the person next to you on the bus is reading exactly the same book.
-We are committed to providing first-class services to the writers who trust us with their books. If we are to fulfil our promises to readers, our job is to do everything in our power to help our writers write the best books they can. We care passionately about the quality of our editorial work. Design and production matter to us because we want our books to be beautiful objects. And since we want our beautiful books to be read on trains and beaches, in beds and planes, or under the desk at school and work, we work up a sweat to promote and market them. And that includes selling rights on behalf of our writers to publishers all around the world. In fact we earn more money for our writers through licensing international editions than we do by turning their books into bestsellers in Australia and New Zealand.
-
-"""
 
 class TextSummarizer:
      #ps = PorterStemmer()
